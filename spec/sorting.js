239c1f2a41c1a98751a6d0af1fa6238f60612634
Implementer <scorpiog@gmail.com>
2013-07-25 21:44:51 +0200

    fix: tests

--- spec/sorting.js
+++ spec/sorting.js
@@ -1,24 +1,62 @@
-for (var method in sorting) {
+describe("insertionSort", function() {
 
-    describe(method, function() {
-
-        it('should sort an empty array', function() {
-            var array = [];
-            sorting[method](array);
-            expect(array.length).toBe(0);
-        });
-
-        it('should sort a single element array', function() {
-            var array = [1];
-            sorting[method](array);
-            expect(array).toEqual([1]);
-        });
-
-        it('should sort an array as expected', function() {
-            var array = [3, 2, 1];
-            sorting[method](array);
-            expect(array).toEqual([1, 2, 3]);
-        });
+    it('should sort an empty array', function() {
+        var array = [];
+        sorting.insertionSort(array);
+        expect(array.length).toBe(0);
     });
 
-}
+    it('should sort a single element array', function() {
+        var array = [1];
+        sorting.insertionSort(array);
+        expect(array).toEqual([1]);
+    });
+
+    it('should sort an array as expected', function() {
+        var array = [3, 2, 1];
+        sorting.insertionSort(array);
+        expect(array).toEqual([1, 2, 3]);
+    });
+});
+
+describe("selectionSort", function() {
+
+    it('should sort an empty array', function() {
+        var array = [];
+        sorting.selectionSort(array);
+        expect(array.length).toBe(0);
+    });
+
+    it('should sort a single element array', function() {
+        var array = [1];
+        sorting.selectionSort(array);
+        expect(array).toEqual([1]);
+    });
+
+    it('should sort an array as expected', function() {
+        var array = [3, 2, 1];
+        sorting.selectionSort(array);
+        expect(array).toEqual([1, 2, 3]);
+    });
+});
+
+describe("quickSort", function() {
+
+    it('should sort an empty array', function() {
+        var array = [];
+        sorting.quickSort(array);
+        expect(array.length).toBe(0);
+    });
+
+    it('should sort a single element array', function() {
+        var array = [1];
+        sorting.quickSort(array);
+        expect(array).toEqual([1]);
+    });
+
+    it('should sort an array as expected', function() {
+        var array = [3, 2, 1];
+        sorting.quickSort(array);
+        expect(array).toEqual([1, 2, 3]);
+    });
+});
