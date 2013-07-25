672a0b4deb3efffb1be59122396577cd933b07d1
Enrico Pelizzon <enrico.pelizzon@gmail.com>
2013-07-25 17:59:27 +0200

    quicksort implementation

--- src/sorting.js
+++ src/sorting.js
@@ -27,8 +27,34 @@ var sorting = (function() {
         }
     };
 
+
+    var quickSortPartitioner = function(array, start, end) {
+        var pivot = array[end-1];
+        var i = start - 1;
+        for (var j = start; j < end-1; ++j) {
+            if (array[j] <= pivot) {
+                ++i;
+                swapArrayElements(array, i, j);
+            }
+        }
+        swapArrayElements(array, i+1, end-1);
+        return i+1;
+    }
+
+    var quickSortMethod = function(array, start, end) {
+        if (end <= start) return;
+        var pivotIndex = quickSortPartitioner(array, start, end);
+        quickSortMethod(array, start, pivotIndex-1);
+        quickSortMethod(array, pivotIndex+1, end);
+    }
+
+    var quickSort = function(array) {
+        quickSortMethod(array, 0, array.length);
+    }
+
     return {
         selectionSort: selectionSort,
-        insertionSort: insertionSort
+        insertionSort: insertionSort,
+        quickSort: quickSort
     };
 })();
